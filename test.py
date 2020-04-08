50fcb075dc02daaccb6a6047d52ed804024e21a1
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-07 22:12:15 -0400

    Testing bonus features for the Hotel App exercise

--- test.py
+++ test.py
@@ -1,33 +1,99 @@
-hotels = {
-    "Hotel California": {
-    "101": {},
-    "102": {},
-    "103": {},
-    "104": {},
-    "105": {},
-}, "Hotel Atlanta": {
-    "101": {},
-    "102": {},
-    "103": {},
-    "104": {},
-    "105": {},
-}, "Hotel Trivago": {
-    "101": {},
-    "102": {},
-    "103": {},
-    "104": {},
-    "105": {},
-}
+import json
+
+Hotel_California = {
+    "Hotel Name": "Hotel California",
+    "101": {
+        "bed type": "king",
+        "pet friendly": True,
+    },
+    "102": {
+        "bed type": "queen",
+        "pet friendly": True,
+    },
+    "103": {
+        "bed type": "double twin",
+        "pet friendly": True,
+    },
+    "104": {
+        "bed type": "king",
+        "pet friendly": False,
+    },
+    "105": {
+        "bed type": "queen",
+        "pet friendly": False,
+    },
+    "106": {
+        "bed type": "double twin",
+        "pet friendly": False,
+    },
 }
 
-list_of_hotels = [hotels]
+Hotel_Atlanta = {
+    "Hotel Name": "Hotel Atlanta",
+    "101": {
+        "bed type": "king",
+        "pet friendly": True,
+    },
+    "102": {
+        "bed type": "queen",
+        "pet friendly": True,
+    },
+    "103": {
+        "bed type": "double twin",
+        "pet friendly": True,
+    },
+    "104": {
+        "bed type": "king",
+        "pet friendly": False,
+    },
+    "105": {
+        "bed type": "queen",
+        "pet friendly": False,
+    },
+    "106": {
+        "bed type": "double twin",
+        "pet friendly": False,
+    },
+}
+
+Hotel_Trivago = {
+    "Hotel Name": "Hotel Trivago",
+    "101": {
+        "bed type": "king",
+        "pet friendly": True,
+    },
+    "102": {
+        "bed type": "queen",
+        "pet friendly": True,
+    },
+    "103": {
+        "bed type": "double twin",
+        "pet friendly": True,
+    },
+    "104": {
+        "bed type": "king",
+        "pet friendly": False,
+    },
+    "105": {
+        "bed type": "queen",
+        "pet friendly": False,
+    },
+    "106": {
+        "bed type": "double twin",
+        "pet friendly": False,
+    },
+}
+
+hotels = [Hotel_California, Hotel_Atlanta, Hotel_Trivago]
+
 main_menu = '''
 
 1. Print hotel room status
 2. Check in customer
 3. Check out customer
 4. Manage hotels
-5. Quit
+5. Manage room data
+6. Quit
 
 '''
 
@@ -40,65 +106,41 @@ manage_hotels = '''
 
 '''
 
-# Dwayne = {
-#     "occupant_name": "Dwayne",
-#     "phone_number": "111-111-1111",
-#     "prepaid": True
-# }
+room_data = '''
 
-# Darrell = {
-#     "occupant_name": "Darrell",
-#     "phone_number": "222-222-2222",
-#     "prepaid": False
-# }
+1. Print hotel list
+2. Save room data
+3. Load room data
+4. Quit
 
-# Dwight = {
-#     "occupant_name": "Dwight",
-#     "phone_number": "333-333-3333",
-#     "prepaid": True
-# }
+'''
 
-# Dilly_Dilly = {
-#     "occupant_name": "Dilly Dilly",
-#     "phone_number": "444-444-4444",
-#     "prepaid": False
-# }
-
-Delilah = {
-    "occupant_name": "Delilah",
-    "phone_number": "555-555-5555",
-    "prepaid": True
-}
-
-# function to check if a room is occupied
-def is_vacant(room):
-    if hotel[room] == {}:
-        print(f"{room} is empty.")
-    else:
-        print(f'{room} is occupied by {hotel[room]["occupant_name"]}.')
-
-# function to assign a person to a room
-def check_in(hotel, room, name):
-    if list_of_hotels[hotels][room] == {}:
-        list_of_hotels[hotels][room] = name
-    else:
-        print(f'{room} is occupied!')
-check_in(0, "105", Delilah)
-check_in(1, "103", Dilly_Dilly)
+# function to check a person into a room
+def check_in(inp):
+    
+    for index in range(len(hotels)):
+        if hotels[index]["Hotel Name"] == inp:
+            inp = index
+            room = input("What room? ")
+            if hotels[index][room] == {}:
+                name = new_customer()
+                hotels[index][room] = name
+            else:
+                print(f'Room {room} is occupied!')
 
 # function to check a person out of the room and return the person dictionary
-def check_out(hotel, room):
-    if hotels[hotel][room]["prepaid"] == True:
-        print("They're paid up! Check 'em out.")
-        if hotels[hotel][room] != {}:
-            guest_info = hotels[hotel][room]
-            hotels[hotel][room] = {}
-            # print(guest_info)
-            return guest_info
-        else:
-            print(f"{room} is empty.")
-    else:
-        print("They owe us money! Hold them hostage forever.")
+def check_out(inp):
+    for index in range(len(hotels)):
+        if hotels[index]["Hotel Name"] == inp:
+            inp = index
+            room = input("What room are you checking out of? ")
+            if hotels[inp][room]["prepaid"] == True:
+                print("They're all paid up! Check them out.")
+                guest_info = hotels[inp][room]
+                hotels[inp][room] = {}
+                return guest_info
+            else:
+                print("They haven't paid! Keep them here forever.")
 
 # function to create a new customer
 def new_customer():
@@ -116,54 +158,126 @@ def new_customer():
     }
     return name
 
+# function to create a new hotel
+def new_hotel():
+    name = input("What's the name of the hotel? ")
+    name = {
+        "Hotel Name": name,
+            "101": {
+                "bed type": "king",
+                "pet friendly": True,
+            },
+            "102": {
+                "bed type": "queen",
+                "pet friendly": True,
+            },
+            "103": {
+                "bed type": "double twin",
+                "pet friendly": True,
+            },
+            "104": {
+                "bed type": "king",
+                "pet friendly": False,
+            },
+            "105": {
+                "bed type": "queen",
+                "pet friendly": False,
+            },
+            "106": {
+                "bed type": "double twin",
+                "pet friendly": False,
+            },
+    }
+    return name
+
+# request room type based off bed
+def request_room_type(inp, room_type):
+    for index in range(len(hotels)):
+        if hotels[index]["Hotel Name"] == inp:
+            inp = index
+            if hotels[inp][room]["bed type"] == room_type:
+                if hotels[inp][room]["bed type"] == 
+                guest_info = hotels[inp][room]
+                hotels[inp][room] = {}
+                return guest_info
+            else:
+                print("They haven't paid! Keep them here forever.")
+
+# request pet friendly room
+def request_pet_friendly(inp):
+
+# save room data
+def save_room_info():
+    file_name = "room_data.json"
+    with open(file_name, "w") as save_file:
+        json.dump(hotels, save_file)
+
+# load room data
+def load_room_info():
+    file_name = "room_data.json"
+    with open(file_name, "r") as file_handle:
+        new_hotels = json.load(file_handle)
+        try:
+            for item in range(len(new_hotels)):
+                hotels[item] = new_hotels[item]
+        except IndexError:
+            hotels.append(new_hotels[item])
+
 while True:
     menu_choice = int(input(main_menu))
-    # Add if/else statements for each menu item
+    # Print list of hotels
     if menu_choice == 1:
         for hotel in hotels:
             print(hotel)
+    # Check in a customer
     elif menu_choice == 2:
         hotel_name = input("What hotel? ")
-        if hotel_name == "Hotel California":
-            hotel_name = 0
-        elif hotel_name == "Hotel Atlanta":
-            hotel_name = 1
-        elif hotel_name == "Hotel Trivago":
-            hotel_name = 2
+        preferance = input("Do you have a bed preferance? ").lower()
+        check_in(hotel_name)
+        if preferance == "yes":
+            rm_type = input("King, Queen, or Double Twin? ").lower()
+            request_room_type(hotel_name, rm_type)
         else:
-            print("No such hotel - sorry!")
             break
-        room = input("What room? ")
-        if hotels[hotel_name][room] == {}:
-            name = new_customer()
-            check_in(hotel_name, room, name)
-            print(hotels)
-        else:
-            print("Room is full!  Pick another one")
+    # Check out a customer
     elif menu_choice == 3:
         hotel_name = input("What hotel are you checking out of? ")
-        if hotel_name == "Hotel California":
-            hotel_name = 0
-        elif hotel_name == "Hotel Atlanta":
-            hotel_name = 1
-        elif hotel_name == "Hotel Trivago":
-            hotel_name = 2
-        else:
-            print("No such hotel - sorry!")
-        room = input("What room are you checking out of? ")
-        check_out(hotel_name, room)
+        check_out(hotel_name)
+    # Manage hotels
     elif menu_choice == 4:
-        menu_choice = int(input(manage_hotels))
-        if menu_choice == 1:
-            print(hotels)
-        elif menu_choice == 2:
-            hotel_name = input("What's the name of the new establishment? ")
-            hotels.append(hotel_name)
-        elif menu_choice == 3:
-            hotel_name = input("Which hotel are we closing? ")
-            del hotels[hotel_name]
-        else:
-            break
+        while True:
+            menu_choice = int(input(manage_hotels))
+            # Print list of hotels
+            if menu_choice == 1:
+                print(hotels)
+            # Add a new hotel
+            elif menu_choice == 2:
+                hotels.append(new_hotel())
+            # Remove a hotel
+            elif menu_choice == 3:
+                hotel_name = input("Which hotel are we closing? ")
+                hotels[:] = [hotel for hotel in hotels if hotel.get('Hotel Name') != hotel_name]
+            else:
+                break
+    # Manage room data
+    elif menu_choice == 5:
+        while True:
+            menu_choice = int(input(room_data))
+            # Print list of hotels
+            if menu_choice == 1:
+                print(hotels)
+            # save room data
+            elif menu_choice == 2:
+                save_room_info()
+                print("Room data has been successfully saved.")
+            # load room info
+            elif menu_choice == 3:
+                load_room_info()
+                print("Room data has been successfully loaded.")
+            # Go back to main loop
+            else:
+                break
+    # Exit the programs
     else:
         break
 print('Thank you for using the Hotels app!')
